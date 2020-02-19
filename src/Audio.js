a333e33d316bf3389c44e70a060c9eafdb54744a
sakamura yosuke <yosuke_sakamura@waku-2.com>
2020-02-19 18:40:20 +0900

    良い感じ

--- src/Audio.js
+++ src/Audio.js
@@ -4,11 +4,32 @@ class Audio {
 		window.AudioContext = window.AudioContext || window.webkitAudioContext;
 		this.ctx = new AudioContext();
 		this.ctx.createGain = this.ctx.createGain || this.ctx.createGainNode;
+
+		this.note = {
+			'C': 261.6,
+			'C#': 277.2,
+			'D': 293.7,
+			'D#': 311.1,
+			'E': 329.6,
+			'F': 349.2,
+			'F#': 370.0,
+			'G': 392.0,
+			'G#': 415.3,
+			'A': 440.0,
+			'A#': 466.2,
+			'B': 493.9,
+			'C2': 523.3,
+		};
+
+		this.oscs = [];
 	}
 
-	startNote() {
+
+	startNote(noteId) {
+		if (!noteId) return;
+
 		var osc = this.ctx.createOscillator();
-		osc.frequency.valu = 261.63;
+		osc.frequency.value = this.note[noteId];
 
 		var gain = this.ctx.createGain();
 		gain.gain.value = 0.3;
@@ -16,6 +37,20 @@ class Audio {
 		osc.connect(gain);
 		gain.connect(this.ctx.destination);
 		osc.start();
+
+		this.oscs.push({
+			note: noteId,
+			osc: osc
+		});
+	}
+
+	stopNote(noteId) {
+		this.oscs.forEach((osc, i, oscs) => {
+			if (osc.note === noteId) {
+				osc.osc.stop();
+				oscs.splice(i, 1);
+			}
+		});
 	}
 }
 
--- src/Listner.js
+++ src/Listner.js
@@ -1,10 +1,41 @@
 /**
+ * KeyCodeを音名にかえる
+ */
+const _getNote = (keyCode) => {
+	console.log(keyCode);
+	switch(keyCode) {
+		case 65: return 'C';
+		case 83: return 'D';
+		case 68: return 'E';
+		case 70: return 'F';
+		case 71: return 'G';
+		case 72: return 'A';
+		case 74: return 'B';
+		case 75: return 'C2';
+		default: return null;
+	}
+}
+
+/**
  * keydown
  */
 const _setOnKeyDown = (audio) => {
-	window.addEventListener('keydown', event => {
-		console.log(event.keyCode);
-		audio.startNote();
+	window.addEventListener('keydown', downEv => {
+		// 押しっぱなしで発火したら処理しない
+		if (downEv.repeat === true) return;
+
+		let note = _getNote(downEv.keyCode);
+		audio.startNote(note);
+
+		document.addEventListener('keyup', onKeyUp);
+
+		// key up
+		function onKeyUp(upEv) {
+			if (upEv.keyCode !== downEv.keyCode) return;
+
+			audio.stopNote(note);
+			document.removeEventListener('keyup', onKeyUp);
+		}
 	});
 };
 
