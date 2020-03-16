258f1d318d31b2fe4c6756efb4b66c24875fcd47
sakamura yosuke <yosuke_sakamura@waku-2.com>
2020-03-16 14:50:18 +0900

    WIP: envelopefilterにreduxを入れる

--- src/components/Chorus.js
+++ src/components/Chorus.js
@@ -2,8 +2,6 @@ import React from 'react';
 
 const Chorus = (props) =>  {
 
-	console.log('chorus', props);
-
 	return(
 		<div className="nes-container with-title is-rounded manipulate__node">
 			<h3 className="title">Chorus</h3>
@@ -17,7 +15,12 @@ const Chorus = (props) =>  {
 					min="0" 
 					step="0.1" 
 					value={props.rate}
-					onChange={e => onRangeChange(e)} />
+					onChange={e => {props.updateChorus(
+						e.target.value,
+						props.time,
+						props.frequency,
+						props.mix
+					)}} />
 			</div>
 			<div>
 				<label className="range-label" htmlFor="chorusTime">TIME</label>
@@ -29,7 +32,12 @@ const Chorus = (props) =>  {
 					min="0" 
 					step="0.01" 
 					value={props.time}
-					onChange={e => onRangeChange(e)} />
+					onChange={e => {props.updateChorus(
+						props.rate,
+						e.target.value,
+						props.frequency,
+						props.mix
+					)}} />
 			</div>
 			<div>
 				<label className="range-label" htmlFor="chorusFrequency">FREQUENCY</label>
@@ -41,7 +49,12 @@ const Chorus = (props) =>  {
 					min="0" 
 					step="0.1" 
 					value={props.frequency}
-					onChange={e => onRangeChange(e)} />
+					onChange={e => {props.updateChorus(
+						props.rate,
+						props.time,
+						e.target.value,
+						props.mix
+					)}} />
 			</div>
 			<div>
 				<label className="range-label" htmlFor="chorusMix">MIX</label>
@@ -53,7 +66,12 @@ const Chorus = (props) =>  {
 					min="0" 
 					step="0.1" 
 					value={props.mix}
-					onChange={e => onRangeChange(e)} />
+					onChange={e => {props.updateChorus(
+						props.rate,
+						props.time,
+						props.frequency,
+						e.target.value
+					)}} />
 			</div>
 		</div>
 	);
