cb392e631686818ade5e2e3ed1270ebced475ef8
sakamura yosuke <yosuke_sakamura@waku-2.com>
2020-03-17 16:49:38 +0900

    patch dataのデータ型変更

---
+++ server/app/migrations/20200317074644-change_patch_data_dataType.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+		return queryInterface('patches', 'data', { dataType: Sequelize.STRING(2048) }); 
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  }
+};
--- server/app/models/patch.js
+++ server/app/models/patch.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
 	},
     name: DataTypes.STRING,
     author: DataTypes.STRING,
-    data: DataTypes.STRING
+    data: DataTypes.STRING(2048)
   }, {
     underscored: true,
   });
