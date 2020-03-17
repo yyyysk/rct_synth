509e181d6fd7e81e4bd76aa91e0bb01fde6d0d0a
sakamura yosuke <yosuke_sakamura@waku-2.com>
2020-03-17 13:16:24 +0900

    update model index

--- server/app/models/index.js
+++ server/app/models/index.js
@@ -8,12 +8,22 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
-const sequelize = new Sequelize('postgres://vfxvxsflqxadeu:27c6ae1cfe615a4b9dfc6af0527cff93ddff0f6e63515f0822c5be5453e06ce5@ec2-34-206-252-187.compute-1.amazonaws.com:5432/de46bj6a8eq5uv');
+// const sequelize = new Sequelize('postgres://vfxvxsflqxadeu:27c6ae1cfe615a4b9dfc6af0527cff93ddff0f6e63515f0822c5be5453e06ce5@ec2-34-206-252-187.compute-1.amazonaws.com:5432/de46bj6a8eq5uv');
 //if (config.use_env_variable) {
 //  sequelize = new Sequelize(process.env[config.use_env_variable], config);
 //} else {
 //  sequelize = new Sequelize(config.database, config.username, config.password, config);
 //}
+if (process.env.HEROKU_POSTGRESQL_BRONZE_URL) {
+    // the application is executed on Heroku ... use the postgres database
+    sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_BRONZE_URL, {
+      dialect:  'postgres',
+      protocol: 'postgres',
+      port:     match[4],
+      host:     match[3],
+      logging:  true //false
+    })  
+}
 
 fs
   .readdirSync(__dirname)
